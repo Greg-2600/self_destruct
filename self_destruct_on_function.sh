c701d5ec0c2af7a92346f1287e67742deaaa4e17
Greg Baker <gregbakerstl@gmail.com>
2019-06-26 13:31:54 -0500

    Create self_destruct_on_function.sh

---
+++ self_destruct_on_function.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# in one function:
+
+self_destruct() {
+    ps aux|              # list all processes
+	  grep $0|             # find this one
+	  grep -v grep|        # remove grep artifact
+	  awk {'print $2'}|    # grab the 2nd item in processlist (pid)
+	  xargs -n1 kill -9 $1 # send a kill signal to myself
+}
+
+
+self_destruct
