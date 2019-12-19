7bf513ccf357c9a4ecd53db2444f57bc296f3fd9
greg baker <gregbakerstl@gmail.com>
2019-12-19 15:48:06 -0600

    shellcheck

--- self_destruct_one_function.sh
+++ self_destruct_one_function.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # in one function:
 
 self_destruct() {
 	ps aux|              # list all processes
-	grep $0|             # find this one
+	grep "$0"|           # find this one
 	grep -v grep|        # remove grep artifact
-	awk {'print $2'}|    # grab the 2nd item in processlist (pid)
-	xargs -n1 kill -9 $1 # send a kill signal to myself
+	awk '{print $2}'|    # grab the 2nd item in processlist (pid)
+	xargs -I {} kill -9 {} # send a kill signal to myself
 }
 
 
